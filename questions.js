8656626f152209e4f55953c3b3c25d48917d4107
Todd MacIntyre <ToddCMacIntyre@gmail.com>
2016-12-02 18:48:46 -0500

    Initial Commit with html, css, and questions frameworks complete.

---
+++ questions.js
@@ -0,0 +1,23 @@
+const questions = [
+	{
+		question: "What is null?",
+		choices: ["object",
+							"primitive data value",
+							"string",
+							"boolean"],
+		answer: 1
+	},
+	{
+		question: "What is an advantage of asynchronous programming?",
+		choices: ["A call to some unit of functionality triggers an action that is allowed to continue outside of the ongoing flow of the program",
+							"The document follows its normal flow of the program",
+							"There are no advantages"],
+		answer: 0
+	},
+	{
+		question: "What does 'TDD' mean in programming?",
+		choices: ["Try doin' dat",
+							"Test Driven Development"],
+		answer: 1
+	}	
+]
